67f25dee4142e1cf3b37519d9b0d2170c8db713c
MariAffini <affini.mariana@unifesp.br>
2026-04-23 14:43:25 -0300

    2758.c

---
+++ 2758.c
@@ -0,0 +1,39 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mariana Sales de Affini
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/2758
+Data        : 14/04/2026
+Objetivo    : Entrada e saída de números reais. 
+Aprendizado : Compreensao do float.
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main() {
+
+ float a, b;
+ double c, d;
+
+ scanf("%f %f", &a, &b);
+ scanf("%lf %lf", &c, &d);
+
+ printf("A = %f, B = %f\n", a, b);
+ printf("C = %lf, D = %lf\n", c, d);
+
+ printf("A = %.1f, B = %.1f\n", a, b);
+ printf("C = %.1lf, D = %.1lf\n", c, d);
+
+ printf("A = %.2f, B = %.2f\n", a, b);
+ printf("C = %.2lf, D = %.2lf\n", c, d);
+
+ printf("A = %.3f, B = %.3f\n", a, b);
+ printf("C = %.3lf, D = %.3lf\n", c, d);
+
+ printf("A = %.3E, B = %.3E\n", a, b);
+ printf("C = %.3E, D = %.3E\n", c, d);
+
+ printf("A = %.0f, B = %.0f\n", a, b);
+ printf("C = %.0lf, D = %.0lf\n", c, d);
+
+    return 0;
+}
