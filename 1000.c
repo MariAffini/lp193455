d24be81a13e45dbaa31cfbceb68ca4e1e9d34697
MariAffini <affini.mariana@unifesp.br>
2026-04-23 14:28:25 -0300

    1000.c

---
+++ 1000.c
@@ -0,0 +1,17 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mariana Sales de Affini
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 23/04/2026
+Objetivo    : Imprimir a palavra "Hello Worl!"
+Aprendizado : Compreendi a ultilização do prinf.
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+ 
+int main() {
+ 
+    printf("Hello World!\n");
+    
+    return 0;
+}
