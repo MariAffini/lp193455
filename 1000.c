d52fb1c14bff68200e2805a11897605065c9873b
MariAffini <affini.mariana@unifesp.br>
2026-04-23 14:45:36 -0300

    1000.c

--- 1000.c
+++ 1000.c
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Mariana Sales de Affini
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1000
-Data        : 23/04/2026
+Data        : 09/04/2026
 Objetivo    : Imprimir a palavra "Hello Worl!"
 Aprendizado : Compreendi a ultilização do prinf.
 -------------------------------------------------------------------------- */
