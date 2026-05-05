9f297f987f7c6eae6e85108dcc94b36afdd92dfb
MariAffini <affini.mariana@unifesp.br>
2026-05-05 15:46:18 -0300

    1241.c

---
+++ 1241.c
@@ -0,0 +1,31 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Mariana Sales de Affini
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1241
+Data        : 05/05/2026
+Objetivo    : Encaixa ou não II
+Aprendizado : compreendi a ultilização do string
+-------------------------------------------------------------------------- */
+#include <string.h>
+#include <stdio.h>
+
+int main(){
+    int N;
+    char A[1001], B[1001];
+
+    scanf("%d\n", &N);
+
+    for(int i = 0; i < N; ++i){
+        scanf("%s %s\n", &A, &B);
+
+        if(strlen(B) > strlen(A))   printf("nao encaixa\n");
+        else{
+            strcpy(A, A + (strlen(A) - strlen(B)));
+            if(!strcmp(A, B))       printf("encaixa\n");
+            else                    printf("nao encaixa\n");
+        }
+    }
+
+    return 0;
+}
